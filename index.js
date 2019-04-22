32ecec263b665e3ea6a72d4ffdb90b75073cd352
tanmv <tanmv@emotiv.com>
2019-04-22 17:27:56 +0700

    done

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ const colors = require('colors');
 const sessions = require("client-sessions");
 const config = require("./config");
 
+const session_client: {
+	cookieName: 'tndata', // cookie name dictates the key name added to the request object
+	secret: '$&38t_$+f52fdgdASF*^', // should be a large unguessable string
+	duration: 60 * 60 * 1000, // how long the session will stay valid in ms
+	activeDuration: 1000 * 60 * 60, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+	cookie: {
+		domain: '.trangnguyen.edu.vn',
+		path: '/', // cookie will only be sent to requests under '/api'
+		// maxAge: 60000, // duration of the cookie in milliseconds, defaults to duration above
+		ephemeral: true, // when true, cookie expires when the browser closes
+		// httpOnly: true, // when true, cookie is not accessible from javascript
+		// secure: false // when true, cookie will only be sent over SSL. use key 'secureProxy' instead if you handle SSL not in your node process
+	}
+};
+
 const getCaptcha = async () => {
 	return await axios.get("https://trangnguyen.edu.vn/captcha.png");
 };
@@ -27,7 +42,7 @@ const getCookie = (res) => {
 };
 
 const decodeSession = (session_string) => {
-	return sessions.util.decode(config.session_client, session_string);
+	return sessions.util.decode(session_client, session_string);
 };
 
 const init = async () => {
@@ -39,7 +54,7 @@ const init = async () => {
 	if (sessionData) {
 		console.log(colors.cyan('Session:'), colors.green(JSON.stringify(sessionData)));
 	} else {
-		console.log(colors.cyan('Secret:'), colors.yellow(config.session_client.secret), colors.red('incorrect'));
+		console.log(colors.cyan('Secret:'), colors.yellow(session_client.secret), colors.red('incorrect'));
 	}
 };
 
